a288c23ef5faab5e3773b575d0782b0041e7c0b1
toritheterrible <torimpham@gmail.com>
2020-06-17 11:14:53 -0700

    setting up the formState

--- src/components/react-regular-form-example/index.js
+++ src/components/react-regular-form-example/index.js
@@ -1,1 +1,17 @@
 import React, { useState } from "react";
+
+export default () => {
+    const [formState, setFormState] = useState({
+      formValues: {
+        email: "",
+        password: ""
+      },
+      formErrors: {
+        email: "",
+        password: ""
+      },
+      formValidity: {
+        email: false,
+        password: false
+      }
+    });
