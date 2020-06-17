8c251b8f816ae8c82cca3ea06fc6da8c3226b4a1
toritheterrible <torimpham@gmail.com>
2020-06-17 11:15:46 -0700

    adding the ui form part

--- src/components/react-regular-form-example/index.js
+++ src/components/react-regular-form-example/index.js
@@ -15,3 +15,54 @@ export default () => {
         password: false
       }
     });
+
+    return (
+        <div className="container">
+          <div className="row mb-5">
+            <div className="col-lg-12 text-center">
+              <h1 className="mt-5">React regular form</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12">
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label>Email address</label>
+                  <input
+                    type="email"
+                    name="email"
+                    className={`form-control ${
+                      formState.formErrors.email ? "is-invalid" : ""
+                    }`}
+                    placeholder="Enter email"
+                    onChange={handleChange}
+                    value={formState.formValues.email}
+                  />
+                  <div className="invalid-feedback">
+                    {formState.formErrors.email}
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    className={`form-control ${
+                      formState.formErrors.password ? "is-invalid" : ""
+                    }`}
+                    placeholder="Password"
+                    onChange={handleChange}
+                    value={formState.formValues.password}
+                  />
+                  <div className="invalid-feedback">
+                    {formState.formErrors.password}
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
