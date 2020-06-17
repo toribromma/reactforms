fd4f9901eb855e49b7ca1a04626a2ac51963a443
toritheterrible <torimpham@gmail.com>
2020-06-17 12:50:40 -0700

    added Formik with email and password

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactBareBone from "./components/react-regular-form-example"
-
+import {Form, Formik, Field, ErrorMessage} from "formik"
 function App() {
+
+  const onSubmit = values => {
+    // form is valid
+    console.log(values);
+  }
+
   return (
     <div>
       <ReactBareBone/>
+
+      <Formik
+        initialValues={{
+          email: "",
+          password: ""
+        }}
+        onSubmit={onSubmit}
+      >
+
+
+      </Formik>
+      
     </div>
   );
 }
